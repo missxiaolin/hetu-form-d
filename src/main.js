85c58dad26e37401d619549aaeedc971e0227332
xiaolin <462441355@qq.com>
2021-02-04 15:55:40 +0800

    * 初始化

--- src/main.js
+++ src/main.js
@@ -4,6 +4,17 @@ import router from './router'
 import store from './store'
 import './plugins/element.js'
 
+// 引入样式
+import './assets/css/index.css'
+
+// 剪切板
+import VueClipboard from 'vue-clipboard2'
+
+// vue 粘贴板复制插件
+VueClipboard.config.autoSetContainer = true
+Vue.use(VueClipboard)
+Vue.prototype.axios = axios
+
 Vue.config.productionTip = false
 
 new Vue({
