139260a9e5de10a07455bacff51210571b5f99c9
xiaolin <462441355@qq.com>
2021-03-26 20:18:34 +0800

    * 拖拽实现

--- src/components/WidgetForm/WidgetForm.vue
+++ src/components/WidgetForm/WidgetForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="data">
-      <el-form
+    <el-form
       :class="(data.config || {}).className"
       :inline="(data.config || {}).inline"
       :label-position="(data.config || {}).labelPosition || 'left'"
@@ -13,18 +13,38 @@
       :validate-on-rule-change="(data.config || {}).validateOnRuleChange"
       :size="(data.config || {}).size || 'small'"
       :disabled="(data.config || {}).disabled"
+    >
+      <vuedraggable
+        style="min-height: 800px"
+        v-model="data.list"
+        v-bind="{ group: 'people', ghostClass: 'ghost' }"
+        @add="handleWidgetAdd"
       >
-      </el-form>
+        <div v-for="(item, index) in (data.list || [])" :key="index" class="vuedraggableitem">
+          <template v-if="item.type === 'grid' || item.type === 'panel'">
+            <widget-form-col
+              :key="item.key"
+              :element.sync="item">
+            </widget-form-col>
+          </template>
+          <template v-else>
+            <widget-form-item
+              :element.sync="item"
+              ></widget-form-item>
+          </template>
+        </div>
+      </vuedraggable>
+    </el-form>
   </div>
 </template>
 
 <script>
-import vuedraggable from 'vuedraggable'
-import { addDraggerWidget } from '../tool/magicGenerate'
+import vuedraggable from "vuedraggable";
+import { addDraggerWidget } from "../tool/magicGenerate";
 // 常规组件
-import WidgetFormItem from './WidgetFormItem.vue'
+import WidgetFormItem from "./WidgetFormItem.vue";
 // 布局组件
-import WidgetFormCol from './WidgetFormCol.vue'
+import WidgetFormCol from "./WidgetFormCol.vue";
 
 export default {
   name: "WidgetForm",
@@ -33,7 +53,8 @@ export default {
     data: {},
   },
   components: {
-      WidgetFormItem
+    WidgetFormItem,
+    vuedraggable
   },
   data() {
     return {};
@@ -42,7 +63,12 @@ export default {
   computed: {},
   created() {},
   mounted() {},
-  methods: {},
+  methods: {
+    handleWidgetAdd ({ newIndex, element }) {
+      let returnItem = addDraggerWidget(this.data.list[newIndex])
+      console.log(returnItem)
+    },
+  },
 };
 </script>
 
