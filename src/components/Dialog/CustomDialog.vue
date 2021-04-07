2a8167d3df8cd92bb549e7989ee14b5c94ec4634
xiaolin <462441355@qq.com>
2021-04-07 20:54:53 +0800

    * 预览初版

--- src/components/Dialog/CustomDialog.vue
+++ src/components/Dialog/CustomDialog.vue
@@ -1,11 +1,49 @@
 <template>
-    <div>
-
-    </div>
+  <div class="">
+    <el-dialog
+      :visible.sync="visbile"
+      title="预览"
+      center
+      :close-on-click-modal="false"
+      destroy-on-close
+      width="1200px"
+    >
+      <!-- 预览 -->
+      <GenerateForm :data="widgetForm" :setting="setting"></GenerateForm>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
+import GenerateForm from "../GenerateForm/GenerateForm.vue";
 export default {
-    name: 'CustomDialog'
-}
+  name: "CustomDialog",
+  // 接收stting
+  inject: ["setting"],
+  components: {
+    GenerateForm,
+  },
+  props: {
+    widgetForm: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+  },
+  data() {
+    return {
+      visbile: false,
+    };
+  },
+  watch: {},
+  computed: {},
+  created() {},
+  mounted() {},
+  methods: {
+    show() {
+      this.visbile = true;
+    },
+  },
+};
 </script>
